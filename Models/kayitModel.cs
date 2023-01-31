5981c35a16a622d5bf111d024a90cd0e731c3c6f
Erhan Ali Yükselen <erhanaly@gmail.com>
2023-01-31 10:56:36 +0300

    Add files via upload

---
+++ Models/kayitModel.cs
@@ -0,0 +1,10 @@
+﻿namespace CRUDApp.Models
+{
+    public class kayitModel
+    {
+        public int kayId { get; set; }
+        public string adsoyad { get; set; }
+        public int yas { get; set; }
+        public string adres { get; set; }
+    }
+}
